7c7279c98bfdab36826892804d3d35784ebb73fe
BitOpenCode <peteranbubit@gmail.com>
2025-11-12 18:52:06 +0300

    Add ESLint config and disable linting in build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    eslint: {
+      lintOnStart: false,
+      failOnError: false,
+    }
+  })],
   server: {
     port: 3001,
     open: true
